eefdc42d3874b774e1a1cab12953bd8cbfd39740
Marcel Šulek <marcel@sulek.eu>
2013-07-30 17:56:16 +0200

    win1250

--- www/js/statnifirmy/base.js
+++ www/js/statnifirmy/base.js
@@ -86,7 +86,7 @@
           if (person[20]) {
             toString += " (" + person[20] + ")";
           }
-          return "<h3>PÅ¯vodnÄ›: </h3>\n<p class='from'>" + fromString + "</p>\n<h3>NÃ¡stupce: </h3>\n<p class='to'>" + toString + "</p>";
+          return "<h3>Pùvodnì: </h3>\n<p class='from'>" + fromString + "</p>\n<h3>Nástupce: </h3>\n<p class='to'>" + toString + "</p>";
         default:
           return "<span class='only'>" + person[7] + " " + person[8] + " " + person[9] + " " + person[10] + "</span>";
         }
@@ -187,9 +187,9 @@
         if (sortMethod === 'importance') {
           switch (it) {
           case 0:
-            return "MinistÅ™i";
+            return "Ministøi";
           case 0.2:
-            return "NÃ¡mÄ›stci";
+            return "Námìstci";
           case 0.4:
             return "Odbory";
           default:
